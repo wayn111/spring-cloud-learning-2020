7f99672bcf00c864ec22208122d8ceaae74f5dc1
waynaqua <166974380@qq.com>
2021-06-18 18:53:33 +0800

    feat(springcloud): ribbon负载

--- cloud-consumer-order80/src/main/java/com/wayn/learn/springcloud/order/OrderApplication80.java
+++ cloud-consumer-order80/src/main/java/com/wayn/learn/springcloud/order/OrderApplication80.java
@@ -1,11 +1,14 @@
 package com.wayn.learn.springcloud.order;
 
+import com.wayn.learn.springcloud.ribbon.MyRuleConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = MyRuleConfig.class)
 public class OrderApplication80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication80.class, args);
