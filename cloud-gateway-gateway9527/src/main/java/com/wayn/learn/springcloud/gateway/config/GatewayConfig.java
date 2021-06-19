187ad6f9633cc04141158a2024c730119414fa2c
waynaqua <166974380@qq.com>
2021-06-19 18:48:39 +0800

    feat(springcloud): gateway使用

---
+++ cloud-gateway-gateway9527/src/main/java/com/wayn/learn/springcloud/gateway/config/GatewayConfig.java
@@ -0,0 +1,24 @@
+package com.wayn.learn.springcloud.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    /**
+     * 自定义网关配置
+     *
+     * @param routeLocatorBuilder
+     * @return
+     */
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("path_route_baidu",
+                r -> r.path("/guonei").uri("http://news.baidu.com")).build();
+        return routes.build();
+    }
+}
