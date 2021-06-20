2a67ddb727b37613521300f770992fd431522979
wayn <1669738430@qq.com>
2021-06-20 10:41:26 +0800

    feat(springcloud): 配置中心搭建

--- cloud-gateway-gateway9527/src/main/java/com/wayn/learn/springcloud/gateway/config/GatewayConfig.java
+++ cloud-gateway-gateway9527/src/main/java/com/wayn/learn/springcloud/gateway/config/GatewayConfig.java
@@ -5,6 +5,8 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.ZonedDateTime;
+
 @Configuration
 public class GatewayConfig {
 
@@ -20,5 +22,9 @@ public class GatewayConfig {
         routes.route("path_route_baidu",
                 r -> r.path("/guonei").uri("http://news.baidu.com")).build();
         return routes.build();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ZonedDateTime.now());
     }
 }
