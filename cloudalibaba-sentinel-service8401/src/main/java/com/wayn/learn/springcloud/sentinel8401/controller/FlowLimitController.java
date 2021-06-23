e7d6bf67ba8a5767e79cc7191073eef1a1035ea9
waynaqua <166974380@qq.com>
2021-06-23 12:56:19 +0800

    feat(springcloud): sentinel使用

--- cloudalibaba-sentinel-service8401/src/main/java/com/wayn/learn/springcloud/sentinel8401/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/wayn/learn/springcloud/sentinel8401/controller/FlowLimitController.java
@@ -1,18 +1,104 @@
 package com.wayn.learn.springcloud.sentinel8401.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class FlowLimitController {
 
+    /**
+     * qps限流
+     *
+     * @return
+     */
     @GetMapping("/testA")
     public String testA() {
         return "------testA";
     }
 
+    /**
+     * qps限流 warmup预热
+     *
+     * @return
+     */
     @GetMapping("/testB")
     public String testB() {
         return "------testB";
+    }
+
+    /**
+     * 线程数限流
+     *
+     * @return
+     */
+    @GetMapping("/testC")
+    public String testC() throws InterruptedException {
+        Thread.sleep(2000);
+        return "------testC";
+    }
+
+    /**
+     * testD-testE关联限流
+     *
+     * @return
+     */
+    @GetMapping("/testD")
+    public String testD() {
+        return "------testD";
+    }
+
+    @GetMapping("/testE")
+    public String testE() {
+        return "------testE";
+    }
+
+    /**
+     * testF排队限流
+     *
+     * @return
+     */
+    @GetMapping("/testF")
+    public String testF() {
+        log.info("------testF");
+        return "------testF";
+    }
+
+    /**
+     * testG 异常数熔断
+     *
+     * @return
+     */
+    @GetMapping("/testG")
+    public String testG() {
+        log.info("------testG");
+        int i = 10 / 0;
+        return "------testG";
+    }
+
+    /**
+     * 热点key限制
+     *
+     * @param p1
+     * @param p2
+     * @return
+     */
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "deal_testHotKey")
+    public String testHotKey(
+            @RequestParam(required = false) String p1,
+            @RequestParam(required = false) String p2) {
+        log.info("------testHotKey");
+        // int i = 10 / 0;
+        return "------testHotKey， p1:" + p1;
+    }
+
+    //兜底方法
+    public String deal_testHotKey (String p1, String p2, BlockException exception){
+        return "------deal_testHotKey,o(╥﹏╥)o";
     }
 }
