5b62e9186027e5ed106f9c082c239d68350c6958
waynaqua <166974380@qq.com>
2021-06-28 17:17:23 +0800

    feat(springcloud): feign使用

--- cloud-consumer-feign-order80/src/main/java/com/wayn/learn/springcloud/feign/order/service/PaymentFeignService.java
+++ cloud-consumer-feign-order80/src/main/java/com/wayn/learn/springcloud/feign/order/service/PaymentFeignService.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@FeignClient("CLOUD-PROVIDER-HYSTRIX-PAYMENT")
+@FeignClient("cloud-payment-service")
 public interface PaymentFeignService {
     @GetMapping(value = "/payment/get/{id}")
     CommonResult getPaymentById(@PathVariable("id") Long id);
