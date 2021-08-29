355e2f3a9bce4219064c31cddc32906c4352b0f0
wayn <1669738430@qq.com>
2021-08-29 15:29:51 +0800

    feat(springcloud): stream使用

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/wayn/learn/springcloud/hystrix/order/service/PaymentFallBackService.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/wayn/learn/springcloud/hystrix/order/service/PaymentFallBackService.java
@@ -1,6 +1,7 @@
 package com.wayn.learn.springcloud.hystrix.order.service;
 
 import org.springframework.stereotype.Component;
+1import org.springframework.web.bind.annotation.GetMapping;
 
 @Component
 public class PaymentFallBackService implements PaymentHystrixService {
@@ -10,6 +11,7 @@ public class PaymentFallBackService implements PaymentHystrixService {
         return "-----PaymentFallbackService fall back-paymentInfo_OK , (┬＿┬)";
     }
 
+    @GetMapping("/payment/hystrix/timeout/{id}")
     @Override
     public String paymentInfo_TimeOut(Integer id) {
         return "-----PaymentFallbackService fall back-paymentInfo_TimeOut , (┬＿┬)";
