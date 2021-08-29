8b6704b7d20ec8c775f0d3c3d8befbd7b7a61f0f
wayn <1669738430@qq.com>
2021-08-29 22:07:21 +0800

    feat(springcloud): sentinel使用

--- cloudalibaba-consumer-nacos-order84/src/main/java/com/wayn/learn/springcloud/order84/controller/CircleBreakerController.java
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/wayn/learn/springcloud/order84/controller/CircleBreakerController.java
@@ -28,7 +28,7 @@ public class CircleBreakerController {
     //@SentinelResource(value = "fallback",fallback = "handlerFallback") //fallback只负责业务异常
     //@SentinelResource(value = "fallback",blockHandler = "blockHandler") //blockHandler只负责sentinel控制台配置违规
     @SentinelResource(value = "fallback", fallback = "handlerFallback", blockHandler = "blockHandler",
-            exceptionsToIgnore = {IllegalArgumentException.class})
+            exceptionsToIgnore = {IllegalArgumentException.class}) // exceptionsToIgnore兜底异常忽略
     public CommonResult<Payment> fallback(@PathVariable Long id) {
         CommonResult<Payment> result = restTemplate.getForObject(SERVICE_URL + "/paymentSQL/" + id, CommonResult.class, id);
 
